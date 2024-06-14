e40ed3ffe96b137e6788dbd549e6a698a2e6ae51
msomea <msomearaphael@gmail.com>
2024-06-14 18:01:52 +0300

    Add Models, Views, urls and templates of accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Manager):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import UserRegistationForm
+from otp.models import OTP
+from otp.views import send_otp_message
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            otp = OTP(user=user)
+            otp.generate_otp()
+            send_otp_sms(form.cleaned_data['phone_number'], otp.otp)
+            request.session['user_id'] = user.id
+            return redirect('otp:verify_otp')
+        else:
+            form = UserRegistationForm()
+    return render(request, 'account/register.html', {'form': form})
+        
