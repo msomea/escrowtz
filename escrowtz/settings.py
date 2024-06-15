2bf817a297c873429a4a61beb1bce6811a5d40ad
msomea <msomearaphael@gmail.com>
2024-06-15 19:33:54 +0300

    upadated static files path in the setting and now all child pages are rendered through extending the base template view and style

--- escrowtz/settings.py
+++ escrowtz/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,6 +52,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+
+# Static directory
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Directory where Django will collect static files (for production)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ROOT_URLCONF = 'escrowtz.urls'
 
